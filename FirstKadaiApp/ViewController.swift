412c083dc725dc019d10d91009c03c1177f2e6b7
yotaroaranami <yotaro.aranami@i.softbank.jp>
2019-06-20 20:50:12 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Yotaro Aranami on 2019/06/20.
+//  Copyright © 2019 Yotaro Aranami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
